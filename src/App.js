e9e818f5f20f1fe40aa9e93f18023eb5b731213c
LatrechOussama <boudy.lat@gmail.com>
2022-01-06 05:11:28 +0100

    react ws

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import mongia from "./image.jpg"
+import video1 from "./video1.mp4"
+import './app1.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Name'>
+    <div className='A' style={{border:'solid 1px black',maxWidth:'100px'}}>
+<img className ='logoo'src='/logo192.png' alt='logoo'/>
+<h1  className='A'>My Name Is Oussama </h1>
+</div>
+<br/> 
+<div className='Imagee'>
+<img style={{width:'600px',height:'400px'}} src="/flying.jpg" alt="logo"/>
+
+<br/> 
+
+<img style={{width:'600px',height:'400px'}} src={mongia} alt='imagee'/>
+</div>
+</div>
+<div className='videoo'>
+<video width="1500" height="240" controls>
+
+<source src={video1} /> 
+
+</video>
+</div>
     </div>
   );
-}
 
+}
 export default App;
